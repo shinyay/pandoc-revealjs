8c2c26c3d2c6d107ff276025d40005cda247c971
shinyay <shinya.com@gmail.com>
2017-02-08 14:08:14 +0900

    Initial Commit
    I added reveal.js and initilize shell

---
+++ init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z ${1} ]; then
+  echo "USAGE: ${0} <DIRECTORY_NAME>" 
+  exit 1
+else
+  DIR_NAME=${1}
+fi
+mkdir -p slides/${DIR_NAME}
+cp -pr reveal.js slides/${DIR_NAME}
+
+cat << EOF > slides/${DIR_NAME}/convert.sh
+#!/bin/bash
+pandoc -s -t revealjs --template=revealjs.custom -V theme:blood -V  transition:page --slide-level=2 slide.md -o slide.html
+EOF
+chmod 755 slides/${DIR_NAME}/convert.sh
+
+cat << EOF > slides/${DIR_NAME}/slide.md
+% Title
+% Name
+% Date
+
+# Main Slide 1
+
+---
+
+### Sub Page
+
+- list
+- list
+
+# Main Slide 2
+
+---
+
+### Sub Slide
+
+1. list
+1. list
+
+EOF
